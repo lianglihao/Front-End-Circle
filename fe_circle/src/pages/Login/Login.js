fe7a9e4b1bb58421a2d82f4c696ad71fa6e7ae7f
lianglihao <807478609@qq.com>
2019-09-26 20:01:53 +0800

    。

--- fe_circle/src/pages/Login/Login.js
+++ fe_circle/src/pages/Login/Login.js
@@ -1,25 +1,36 @@
 import React, { Component, Fragment } from 'react'
 import { Link } from 'react-router-dom'
-// import { observable, action } from 'mobx'
-import { observer, inject } from 'mobx-react'
 
-@inject('UserStore')
-@observer
+// import { observable, action } from 'mobx'
+// import { observer, inject } from 'mobx-react'
+
+// @inject('UserStore')
+// @observer
 class Login extends Component {
 
-  setUser() {
-    const { UserStore, history } = this.props
-    UserStore.signIn()
-    history.push('/')
+  componentDidMount() {
+    // document.title = '登录页'
+    localStorage.removeItem('login')
   }
+
+  // setUser() {
+  //   const { UserStore, history } = this.props
+  //   UserStore.signIn()
+  //   history.push('/')
+  // }
 
   login() {
     localStorage.setItem('login', 1)
-    this.setUser()
+    // this.setUser()
   }
 
   logout() {
     localStorage.removeItem('login')
+  }
+
+  goHome() {
+    // window.location.replace('/')
+    // console.log(this.props)
   }
 
   render() {
@@ -28,6 +39,9 @@ class Login extends Component {
       <Fragment>
         <p>Login</p>
         <Link to="/">goHome</Link>
+        <button onClick={this.goHome}
+          type="primary"
+        >Primary</button>
         <button onClick={this.login}>登陆</button>
         <button onClick={this.logout}>退出</button>
       </Fragment>
